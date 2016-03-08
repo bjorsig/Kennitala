7d0654f1fe33ebc0875c045121a6d3258be90082
Bjartur Thorlacius <bjt9@hi.is>
2016-03-08 13:16:14 +0000

    von og óvon um gilda kennitölu

--- hsKennitala/Kennitala.hs
+++ hsKennitala/Kennitala.hs
@@ -2,15 +2,6 @@ module Kennitala where
 import Text.Read
 import Control.Applicative
 
-tala :: String -> Char -> Char -> Int
-tala heiti a b = either (error $ heiti ++ " má einungis innihalda tölustafi.") id $ readEither [a,b]
-
-tölustaf :: String -> Char -> Int
-tölustaf heiti a = let t = fromEnum a - fromEnum '0'
-    in if   t > 9
-       then error (heiti ++ " á að vera tölustafur.")
-       else t
-
 data Kennitala = Kennitala {
                    daggildi :: Int,
                    mánuður :: Int,
@@ -31,6 +22,18 @@ instance Show Kennitala where
     show (Kennitala daggildi mánuður ár nr vartala öld) =
         (show =<< [daggildi,mánuður,ár])  ++ "-" ++ (show =<< [nr,vartala,öld])
 
+tala :: String -> Char -> Char -> Int
+tala heiti a b = either (error $ heiti ++ " má einungis innihalda tölustafi.") id $ readEither [a,b]
+
+tölustaf :: String -> Char -> Int
+tölustaf heiti a = let t = fromEnum a - fromEnum '0'
+    in if   t > 9
+       then error (heiti ++ " á að vera tölustafur.")
+       else t
+
+vona :: (Monad m) => Bool -> String -> Int -> m Kennitala -> m Kennitala
+vona condition melding gildi kt = if not condition then fail (melding ++ "—Ekki " ++ show gildi ++ ".") else kt
+
 kennitala :: (Monad m) => String -> m Kennitala
 kennitala [dagtugar,dags,mántugar,mánuðar,áratugar,árs,nr1,nr2,vartölu,aldar] =
     let dagur   = tala "Dagsetning" dagtugar dags
@@ -40,9 +43,10 @@ kennitala [dagtugar,dags,mántugar,mánuðar,áratugar,árs,nr1,nr2,vartölu,ald
         vartala = tölustaf "Vartala" vartölu
         öld     = tölustaf "Öld" aldar
     in
-        if dagur > 71 || (31 < dagur && dagur < 41) then fail "Mánaðardagur má í mesta lagi vera 71 (eða 31 fyrir einstaklinga)."  else
-        if  mánuður > 12 then fail "Mánuðirnir eru ekki nema tólf."                                     else
-        if       nr < 20 then fail "Einstaklingar eru númeraðir frá 20."                                else
+        vona (dagur <= 71) "Daggildi má mest vera 71." dagur $
+        vona (dagur <= 31 || 41 <= dagur) "Daggildi má mest vera 31 fyrir einstaklinga en minnst 41 fyrir félög." dagur $
+        vona  (mánuður <= 12) "Mánuðirnir eru ekki nema tólf." mánuður$
+        vona  (nr >= 20)     "Einstaklingar eru númeraðir frá 20." nr $
         return $ Kennitala dagur mánuður ár nr vartala öld
 kennitala (dagtug:dag:mántug:mánuð:áratug:ár':'-':rest) = kennitala (dagtug:dag:mántug:mánuð:áratug:ár':rest)
 kennitala _ = fail "Kennitala þarf að vera 10 tölustafir, að frátöldu valfrjálsu bandstriki."
@@ -64,4 +68,6 @@ vartölu listi = if length listi /= 8
         else Just niðurstaða
 
 
-main = (print =<< kennitala "211295-2019") >> print (vartölu [2,1,1,2,9,5,2,0]) 
+main = (print =<< kennitala "211295-2019")
+     >> putStr "Vartala: "
+     >> maybe (error "Vartala kemst ekki fyrir í einum tölustaf") print (vartölu [2,1,1,2,9,5,2,0])
