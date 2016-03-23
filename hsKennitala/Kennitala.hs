0d884c4232113e1ffb5f0efaeb30ab9c2979ff59
Bjartur Thorlacius <bjt9@hi.is>
2016-03-23 14:49:09 +0000

    Either (Maybe)

--- hsKennitala/Kennitala.hs
+++ hsKennitala/Kennitala.hs
@@ -1,6 +1,7 @@
 module Kennitala where
 import Text.Read
 import Control.Applicative
+import Data.Traversable
 
 data Kennitala = Kennitala {
                    daggildi :: Int,
@@ -22,34 +23,45 @@ instance Show Kennitala where
     show (Kennitala daggildi mánuður ár nr vartala öld) =
         (show =<< [daggildi,mánuður,ár])  ++ "-" ++ (show =<< [nr,vartala,öld])
 
-tala :: String -> Char -> Char -> Int
-tala heiti a b = either (error $ heiti ++ " má einungis innihalda tölustafi. Ekki " ++ [a] ++ " og " ++ [b] ++ ".") id $ readEither [a,b]
+breytaVillu :: (a -> b) -> Either a r -> Either b r
+breytaVillu f = either (Left . f) Right
 
-tölustaf :: String -> Char -> Int
+x & f = f x
+
+tala :: String -> (Int -> Bool) -> Char -> Char -> String -> Either String Int
+tala heiti skilyrði a b melding = (readEither [a,b]
+    & breytaVillu (const $ heiti ++ " má einungis innihalda tölustafi")
+    >>= \gildi ->
+        if skilyrði gildi
+            then Right gildi
+            else Left melding)
+    & breytaVillu (++ ", ekki " ++ [a,b] ++ ".")
+
+tölustaf :: String -> Char -> Either String Int
 tölustaf heiti a = let gildi = fromEnum a - fromEnum '0'
     in if   gildi > 9
-       then error (heiti ++ " á að vera tölustafur.")
-       else gildi
+       then Left $ heiti ++ " á að vera tölustafur, ekki " ++ [a] ++ "."
+       else Right gildi
 
-vona :: (Monad m) => Bool -> String -> Int -> m Kennitala -> m Kennitala
-vona condition melding gildi kt = if not condition then fail (melding ++ "—Ekki " ++ show gildi ++ ".") else kt
-
-kennitala :: (Monad m) => String -> m Kennitala
-kennitala [dagtugar,dags,mántugar,mánuðar,áratugar,árs,nr1,nr2,vartölu,aldar] =
-    let dagur   = tala "Dagsetning" dagtugar dags
-        mánuður = tala "Mánuður" mántugar mánuðar
-        ár      = tala "Ártal" áratugar árs
-        nr      = tala "Númer" nr1 nr2
-        vartala = tölustaf "Vartala" vartölu
-        öld     = tölustaf "Öld" aldar
-    in
-        vona (dagur <= 71) "Daggildi má mest vera 71." dagur $
-        vona (dagur <= 31 || 41 <= dagur) "Daggildi má mest vera 31 fyrir einstaklinga en minnst 41 fyrir félög." dagur $
-        vona  (mánuður <= 12) "Mánuðirnir eru ekki nema tólf." mánuður$
-        vona  (nr >= 20)     "Einstaklingar eru númeraðir frá 20." nr $
-        return $ Kennitala dagur mánuður ár nr vartala öld
+kennitala :: String -> Either String Kennitala
+kennitala [dagtugur,dagur,mántugur,mánuður,áratugur,ár,nr1,nr2,vartala,öld] =
+    Kennitala <$>
+    tala "Dagsetning" (\dagur -> dagur <= 71 && (dagur <= 31 || 41 <= dagur) ) dagtugur dagur
+        "Daggildi má mest vera 31 fyrir einstaklinga en minnst 41 fyrir félög. Daggildi má vera margt að 71" <*>
+    tala "Mánuður" (<= 12) mántugur mánuður "Mánuðirnir eru tólf" <*>
+    tala "Ártal" (const True) áratugur ár "Ár má tákna með hvaða tveggja stafa tölu sem er" <*>
+    tala "Númer" (>= 20) nr1 nr2 "Einstaklingar eru númeraðir frá 20" <*>
+    tölustaf "Vartala" vartala <*>
+    tölustaf "Öld" öld
+    >>= \kt ->
+    vartölu [dagtugur,dagur,mántugur,mánuður,áratugur,ár,nr1,nr2]
+        >>= maybe (Left "Þessi kennitala getur ekki verið rétt. Hún á sér ekki vartölu.")
+        (\útreiknuð ->
+            if útreiknuð == vartala
+                then Right kt
+                else Left $ "Vartalan " ++ [vartala] ++ " stemmir ekki við útreiknaða vartölu " ++ show útreiknuð ++ ".")
 kennitala (dagtug:dag:mántug:mánuð:áratug:ár':'-':rest) = kennitala (dagtug:dag:mántug:mánuð:áratug:ár':rest)
-kennitala _ = fail "Kennitala þarf að vera 10 tölustafir, að frátöldu valfrjálsu bandstriki."
+kennitala _ = Left $ "Kennitala þarf að vera 10 tölustafir, að frátöldu valfrjálsu bandstriki."
 
 -- Flestar 8 tölustafa runur eiga sér vartölu.
 -- Stundum kemst út reiknuð vartala ekki fyrir í einum tölustaf.
@@ -58,17 +70,19 @@ kennitala _ = fail "Kennitala þarf að vera 10 tölustafir, að frátöldu valf
 -- Annaðhvort var runan rangt slegin inn, eða Þjóðskrá verður að velja nýtt númer.
 -- Ef út reiknuð vartala kemst fyrir, þá skilar fallið Just henni.
 vartöluFastar = 3:2:[7,6..2]
-vartölu :: [Int] -> Maybe Int
-vartölu listi = if length listi /= 8
-    then error "Vartala reiknast ekki nema af fyrstu 8 stöfum kennitölu."
-    else let niðurstaða = 11 - ((sum $ zipWith (*) vartöluFastar listi) `mod` 11)
-    in
-        if niðurstaða == 11 then Just 0
-        else if niðurstaða == 10 then Nothing
-        else Just niðurstaða
+vartölu :: String -> Either String (Maybe Char)
+vartölu strengur = if length strengur /= 8
+    then Left "Vartala reiknast ekki nema af fyrstu 8 stöfum kennitölu."
+    else do 
+            listi <- traverse (tölustaf "Tölustafur") strengur :: Either String [Int]
+            let niðurstaða = 11 - ((sum $ zipWith (*) vartöluFastar listi) `mod` 11)
+            return . fmap (toEnum . (fromEnum '0' +)) $
+                if niðurstaða == 11 then  Just 0
+                else if niðurstaða == 10 then Nothing
+                else Just niðurstaða
 
 
-main = (print =<< kennitala "211295-2019")
+main = (either error print $ kennitala "211295-2019")
      >> putStr "Vartala: "
-     >> maybe (error "Vartala kemst ekki fyrir í einum tölustaf") print (vartölu [2,1,1,2,9,5,2,0])
-     >>(print =<< kennitala "2112b5-2019")
+     >> either error (maybe (error "Vartala kemst ekki fyrir í einum tölustaf") print) (vartölu "21129520")
+     >> (either error print $ kennitala "21129b-2019")
